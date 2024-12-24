34b7de919249217ea82a56081e154184ff668f3a
manfred messing <mm@mm-ing.de>
2024-12-24 16:20:04 +0100

    loco finished

--- Lesson02/Loco.py
+++ Lesson02/Loco.py
@@ -22,32 +22,6 @@ canvas = Canvas(root, width=canvasWidth, height=canvasHeight, bg=background)
 # create an instance of Draw
 df = Draw(canvas)
 
-# def create_circle(center_x, center_y, radius, outline_color, width):  
-#     x, y = center_x, center_y
-#     r = radius
-#     circle = canvas.create_oval(x-r, y-r, x+r, y+r, outline=outline_color, fill='red', width=width)
-#     return circle
-
-# def create_ortho_lines(line_list, line_length, x, y):
-#     loco = []
-#     for line in line_list:
-#         s_x = x
-#         s_y = y
-#         e_x = s_x
-#         e_y = s_y
-#         if line[0] == 'v':
-#             e_y = s_y + line_length * line[1]
-#         if line[0] == 'h':
-#             e_x = s_x + line_length * line[1]
-#         if line[0] == 'vh':
-#             e_x = s_x + line_length * line[1]
-#             e_y = s_y + line_length * line[1]
-#         # create line
-#         loco.append(canvas.create_line(s_x, s_y, e_x, e_y, fill='blue', width=2))
-#         x = e_x
-#         y = e_y    
-#     return [x, y, loco]
-
 def create_loco_body(lower_left_x, lower_left_y, line_length):
     geometry = []
     offset = 0     
@@ -67,10 +41,10 @@ def run_loco():
     loco = create_loco_body(locoStart[0], locoStart[1], seg_length)
     # smoke pipe
     coord = locoStart[0] + 6 * seg_length, locoStart[1] - 6 * seg_length, locoStart[0] + 8 * seg_length, locoStart[1] - 4 * seg_length
-    circles.append(df.create_arc(coord[0], coord[1], coord[2], coord[3], 0, 180)) 
+    circles.append(df.create_arc(coord[0], coord[1], coord[2], coord[3], 0, 180, fill_color='red')) 
     # window
-    coord = locoStart[0] + 1 * seg_length, locoStart[1] - 1 * seg_length, locoStart[0] + 3 * seg_length, locoStart[1] - 3 * seg_length
-    circles.append(df.create_rectangle(locoStart[0] + 1 * seg_length, locoStart[1] - 3 * seg_length, locoStart[0] + 3 * seg_length, locoStart[1] - 1 * seg_length))
+    coord = locoStart[0] + 1 * seg_length, locoStart[1] - 4 * seg_length, locoStart[0] + 3 * seg_length, locoStart[1] - 6 * seg_length
+    circles.append(df.create_rectangle(coord[0], coord[1], coord[2], coord[3]))
     # wheel 1
     circles.append(df.create_circle(locoStart[0] + 3 * seg_length, locoStart[1], seg_length))
     # wheel 2
