acd3d4faacf2f8ad68d21f19cc4197f5932a504e
manfred messing <mm@mm-ing.de>
2025-01-09 13:02:13 +0100

    expansions

---
+++ Lesson01/MathHelp.py
@@ -0,0 +1,7 @@
+# Import the 'math' module to access mathematical functions and constants.
+import math
+# Print a message to provide details about the 'math' module.
+print("Details of math module:\n")
+# Use the 'help' function to display the documentation and details of the 'math' module.
+# The 'help' function will show information about available functions, constants, and how to use them.
+help(math)
